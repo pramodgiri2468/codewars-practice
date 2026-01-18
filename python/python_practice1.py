c4f14784a32bf6452501f06f48c2b7b1619a483e
Pramod Giri <pramod@Pramods-MacBook-Pro.local>
2026-01-18 20:44:24 +0545

    Solved

---
+++ python/python_practice1.py
@@ -0,0 +1,22 @@
+def count_by(x, n):
+    """
+    Generate a list of multiples of a given number.
+
+    Parameters:
+    x (int): The number whose multiples are to be generated.
+    n (int): The number of multiples to generate.
+
+    Returns:
+    list: A list containing the first `n` multiples of `x`.
+
+    Example:
+    >>> count_by(2, 5)
+    [2, 4, 6, 8, 10]
+    """
+    result = []
+
+    for i in range(1, n + 1):
+        result.append(x * i)
+
+    return result  
+print(count_by(2, 5))  # Example usage
